3c8b501e3d06fe6588dc4af77a62b2ff4f4a32e7
nina <icefrog911@yahoo.com>
2018-05-13 20:09:26 -0500

    add http-request-printer

---
+++ routes/http-request-printer.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('http-request-printer', {method: req.method, url: req.baseUrl, headers: req.headers, httpVersion: req.httpVersion, query: req.query});
+});
+
+
+module.exports = router;
