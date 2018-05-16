6a93f5dd50055857de0b91bacba19e12005eaaa2
nina <icefrog911@yahoo.com>
2018-05-15 20:56:23 -0500

    add text-form

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var http = require('./routes/http-request-printer');
-
+var textForm = require('./routes/text-form');
 var app = express();
 
 // view engine setup
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/http-request-printer', http);
+app.use('/text-form', textForm );
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
