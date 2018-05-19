da480c46e4e1a49ffdb949c2932e7ab197f1acaf
nina <icefrog911@yahoo.com>
2018-05-19 17:29:57 -0500

    add text analyzer

--- routes/text-form.js
+++ routes/text-form.js
@@ -8,7 +8,26 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
     console.log(req.body);
-    res.render('text-form', {title: 'text-form', userText: req.body.message});
+    var userText = req.body.message;
+
+    function splitMulti(str, tokens) {
+        var tempChar = tokens[0];
+        for (var i = 1; i < tokens.length; i++) {
+            str = str.split(tokens[i]).join(tempChar);
+        }
+        str = str.split(tempChar);
+        return str;
+    }
+
+    var sentences = splitMulti(userText, ['.', '!', '?', '...']);
+    res.render('text-form', {
+        title: 'text-form',
+        userText: req.body.message,
+        wordTokens: req.body.message.split(' '),
+        symbolTokens: req.body.message.split(/\s*/),
+        sentences: sentences
+    });
+
 });
 
 
